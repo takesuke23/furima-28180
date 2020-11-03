c287cb62a05a09cdfa18b547719d551fa0b9fa99
takesuke23 <takesuke.23@gmail.com>
2020-11-03 14:36:53 +0900

    バリデーションとappコントローラーの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :kana_last, presence: true
+  validates :kana_first, presence: true
 end
