7c6031ccbaa15fe8776ec68e701d917fca4995d2
takesuke23 <takesuke.23@gmail.com>
2020-11-05 16:38:42 +0900

    itemsコントローラーを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
   before_action :configre_permitted_parameters, if: :devise_controller?
 
   def configre_permitted_parameters
