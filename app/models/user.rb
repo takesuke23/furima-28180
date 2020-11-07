b77c791421f574090b5ffd84fb591f7ccafe1391
takesuke23 <takesuke.23@gmail.com>
2020-11-07 16:17:53 +0900

    パスワードの半角英数字の記述変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :email, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{6,}+\z/
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
+  # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, presence: true,  format: { with: VALID_PASSWORD_REGEX}
   validates :last_name, :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
   validates :kana_last, :kana_first, presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birthday, presence: true
