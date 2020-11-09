3e691668d70ccf7079028b2e93b4dc2ed7fdbc25
takesuke23 <takesuke.23@gmail.com>
2020-11-09 15:38:23 +0900

    with_optionsで共通項目をまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
-  validates :email, presence: true
-  # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]+\z/
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :birthday
+  end
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, presence: true,  format: { with: VALID_PASSWORD_REGEX}
   validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :kana_last, :kana_first, presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/ }
-  validates :birthday, presence: true
 end
