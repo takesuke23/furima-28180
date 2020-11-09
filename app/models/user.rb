25046eb431b96e8ef52127080492116dc638f6b2
takesuke23 <takesuke.23@gmail.com>
2020-11-09 14:49:48 +0900

    バリデーション設定変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]+\z/
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, presence: true,  format: { with: VALID_PASSWORD_REGEX}
-  validates :last_name, :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :kana_last, :kana_first, presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birthday, presence: true
 end
