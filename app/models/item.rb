aca897d9c0295ac26e2e0cb05860c56a7e034ff8
takesuke23 <takesuke.23@gmail.com>
2020-11-11 17:43:50 +0900

    ActiveHashを用いるファイルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtentions
-  belongs_to :prefercture
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :day
-  belongs_to :ship
-  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :day
+  belongs_to_active_hash :ship
+  belongs_to_active_hash :user
   with_options precense: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
@@ -18,6 +18,6 @@ class Item < ApplicationRecord
     validates :name
     validates :description  
   end
-  validates :price, precense: true, format: {with: /\A[0-9]{300,9999999}+\z/}
+  validates :price, precense: true, format: {with: /\A[0-9]{300,1000}+\z/}
   validates :user_id, precense: true, foreign_key: true
 end
