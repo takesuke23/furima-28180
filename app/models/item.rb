926bd9e542d0f792ee43d98f2ad7a39988e3ba68
takesuke23 <takesuke.23@gmail.com>
2020-11-11 11:26:17 +0900

    各モデルのバリデーションとアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,23 @@
 class Item < ApplicationRecord
-  with_options precense: true do
-    validates :name
-    validates :description
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtentions
+  belongs_to :prefercture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :day
+  belongs_to :ship
+  belongs_to :user
+  with_options precense: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
     validates :ship_id
-    validates :pre_id
-    validates :days_id
-    validates :price
+    validates :prefecture_id
+    validates :day_id
   end
+  with_options precense: true do
+    validates :name
+    validates :description  
+  end
+  validates :price, precense: true, format: {with: /\A[0-9]{300,9999999}+\z/}
   validates :user_id, precense: true, foreign_key: true
-  belongs_to :user
-  has_one_attached :image
 end
