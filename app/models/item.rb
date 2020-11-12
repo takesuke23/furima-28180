7b781e8ebac538619b542085c7dc5a04a67ca91a
takesuke23 <takesuke.23@gmail.com>
2020-11-12 22:15:45 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,17 +7,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :day
   belongs_to_active_hash :ship
   belongs_to_active_hash :user
-  with_options precense: true, numericality: { other_than: 1 } do
+  with_options presence: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
     validates :ship_id
     validates :prefecture_id
     validates :day_id
   end
-  with_options precense: true do
+  with_options presence: true do
     validates :name
-    validates :description  
+    validates :description
   end
-  validates :price, precense: true, format: {with: /\A[0-9]{300,1000}+\z/}
-  validates :user_id, precense: true, foreign_key: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/ }
+  validates :user_id, presence: true
 end
